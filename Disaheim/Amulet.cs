c3f40dbcbcffb5bdbecf21c12a49304b33cfc0b2
Phamezan <pham2003@outlook.dk>
2024-10-10 10:49:57 +0200

    Første commit

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+
+        public string ItemId { get; set; }
+        public Level Quality { get; set; }
+        public string Design { get; set; }
+
+        public Amulet(string itemId) : this (itemId, Level.medium,"")
+        {
+        }
+
+
+        public Amulet(string itemId, Level quality)
+        {
+            ItemId = itemId;
+            Quality = quality;
+        }
+        public Amulet(string itemId, Level quality, string design)
+        {
+            ItemId = itemId;
+            Design = design;
+            Quality = quality;
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: {ItemId}, Quality: {Quality}, Design: {Design}";
+        }
+    }
+}
+
